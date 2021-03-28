15ff078ef2def0dc95281c3c1c505106b22d7166
Steven_Klavins <stevenklavins94@gmail.com>
2021-03-28 11:35:55 +0100

    chore: added fuctionality to stop game loop

--- shooting-range.py
+++ shooting-range.py
@@ -4,3 +4,12 @@ import pygame
 pygame.init()
 # Create screen/canvas + resolution
 screen = pygame.display.set_mode((1280, 720))
+
+# While True is our continuous game loop
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: 
+           pygame.quit()   # This event allows us to close window, without this the window wont close
+           sys.exit() # Finalises the closing of the game completely (kills all)
+
+    pygame.display.update() # This event continuously updates frames
